59a96affe3e39023b285b08294b3d42616555a13
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-03-13 21:15:46 -0400

    commit

--- src/Word.java
+++ src/Word.java
@@ -1,1 +1,19 @@
 import java.util.ArrayList;
+
+public class Word {
+	
+	public String word;
+	public ArrayList<Definition> definitions; 
+	public ArrayList<String> synonyms;
+	public ArrayList<String> antonyms;
+	
+	public Word() {
+		
+	}
+	
+	public Word(String name, ArrayList<Definition> definitions, ArrayList<String> synonyms, ArrayList<String> antonyms) {
+		this.word = name;
+		this.definitions = definitions;
+		this.synonyms = synonyms;
+		this.antonyms = antonyms;
+	}
