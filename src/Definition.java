9f15381c1daad8ae13d779af6e4cea1688b92a78
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-03-13 21:01:39 -0400

    Definition done

---
+++ src/Definition.java
@@ -0,0 +1,29 @@
+public class Definition {
+
+	private String definition;
+	private String partOfSpeech;
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the application.
+	 */
+	public Definition(String definition, String partOfSpeech) {
+		this.definition = definition;
+		this.partOfSpeech = partOfSpeech;	
+	}
+	
+	public String getDefinition() {
+		return definition;
+	}
+	
+	public String getPartOfSpeech() {
+		return partOfSpeech;
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+}
