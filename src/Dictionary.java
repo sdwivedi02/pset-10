28993624ef184c1d5e9b49466c62eef8b77b2fdc
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-03-13 21:08:37 -0400

    Dictionary halfway done?

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -84,3 +84,93 @@ class Dictionary {
 		return null;
 
 	}
+	
+	public static String formatDefinitions(ArrayList<Definition> defs) {
+		String formattedDefinitions = "";
+		String nounDefinitions = "Noun: \n";
+		String verbDefinitions = "Verb: \n";
+		String adjectiveDefinitions = "Adjective: \n";
+		String nullDefinitions = "Uncategorized: \n";
+
+		for (int i = 0; i < defs.size(); i++) {
+
+			if (defs.get(i).getPartOfSpeech() != null) {
+
+				if (defs.get(i).getPartOfSpeech().equals("noun")) {
+
+					nounDefinitions += "\t- " + defs.get(i).getDefinition() + "\n";
+
+				} else if (defs.get(i).getPartOfSpeech().equals("verb")) {
+
+					verbDefinitions += "\t- " + defs.get(i).getDefinition() + "\n";
+
+				} else if (defs.get(i).getPartOfSpeech().equals("adjective")) {
+
+					adjectiveDefinitions += "\t- " + defs.get(i).getDefinition() + "\n";
+
+				}
+			} else if (defs.get(i).getPartOfSpeech() == null) {
+
+				nullDefinitions += "\t- " + defs.get(i).getDefinition() + "\n";
+
+			}
+		}
+
+		if (!(nounDefinitions.equals("Noun: \n"))) {
+
+			formattedDefinitions += "\n" + nounDefinitions;
+
+		}
+
+		if (!(verbDefinitions.equals("Verb: \n"))) {
+
+			formattedDefinitions += "\n" + verbDefinitions;
+
+		}
+
+		if (!(adjectiveDefinitions.equals("Adjective: \n"))) {
+
+			formattedDefinitions += "\n" + adjectiveDefinitions;
+
+		}
+
+		if (!(nullDefinitions.equals("Uncategorized: \n"))) {
+
+			formattedDefinitions += "\n" + nullDefinitions;
+
+		}
+
+		return formattedDefinitions;
+	}
+
+	public static String getFormattedWordDefinitions(int index) {
+
+		String formattedDefinitions = formatDefinitions(getDefinitions(getNames().get(index)));
+
+		return formattedDefinitions;
+
+	}
+
+	public static void editJson(ArrayList<Word> words) {
+		try {
+
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			;
+
+			Reader reader = Files.newBufferedReader(Paths.get("src/words.json"));
+
+			String wordsJson = gson.toJson(words);
+
+			FileWriter fileWriter = new FileWriter("src/words.json");
+
+			fileWriter.write(wordsJson);
+			fileWriter.close();
+
+			// close reader
+			reader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+	}
