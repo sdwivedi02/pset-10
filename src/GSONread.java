3794ba4c38ce00cd434b55428c9d8ced768c265f
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-03-13 21:32:27 -0400

    GSON finished (?)

--- src/GSONread.java
+++ src/GSONread.java
@@ -8,5 +8,45 @@ import java.util.List;
 import com.google.gson.Gson;
 
 public class GSONread {
-	
+	static Word[] wordObj;
+
+	public static void main(String[] args) {
+		returnWords(true);
+	}
+
+	public static ArrayList<Word> returnWords(boolean aToZ) {
+		Gson gson = new Gson();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("words.json"));
+
+			List<Word> list = Dictionary.getWords();
+
+			Collections.sort(list, new Comparator<Word>() {
+				@Override
+				public int compare(Word word2, Word word1) {
+
+					return word1.word.compareTo(word2.word);
+				}
+			});
+
+			ArrayList<Word> sortedWords = new ArrayList(list);
+
+			if (aToZ) {
+				ArrayList<Word> temp = new ArrayList<Word>();
+				for (int i = sortedWords.size() - 1; i > -1; i--) {
+					temp.add(sortedWords.get(i));
+				}
+				sortedWords = temp;
+			}
+
+			Dictionary.editJson(sortedWords);
+
+			return sortedWords;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
